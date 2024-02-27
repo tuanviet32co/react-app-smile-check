eeb39ea452301fbddbbd7b38b8e1d4d7e01d2ed7
kiennguyen <>
2024-02-27 14:59:28 +0700

    add image compare

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { useState } from 'react';
 import { BeforeAfterImages } from './components/before-after-images';
 import { ImageDragger } from './components/image-dragger';
 import { Form } from 'antd';
+import before from './assets/before.jpg';
+import after from './assets/after.jpg';
 
 function App() {
-  const [before, setBefore] = useState<string>('');
-  const [after, setAfter] = useState<string>('');
+  // const [before, setBefore] = useState<string>('');
+  // const [after, setAfter] = useState<string>('');
 
   return (
-    <div className=' min-h-screen'>
-      <div className='px-4 py-6'>
+    <div className=" min-h-screen">
+      <div className="px-4 py-6">
         <Form.Item>
           <ImageDragger name="" />
         </Form.Item>
-        <BeforeAfterImages before='0' after='0' />
+        <BeforeAfterImages before={before} after={after} />
       </div>
     </div>
   );
